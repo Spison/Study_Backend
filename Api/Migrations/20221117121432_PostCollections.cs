8364967644f62a67c068d5ba9151eee824ef4711
Spison <sanya.kanash@yandex.ru>
2022-11-17 15:14:58 +0300

    Догрузил миграцию

---
+++ Api/Migrations/20221117121432_PostCollections.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    public partial class PostCollections : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
